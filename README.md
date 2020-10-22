e45964e7ab4f8694c56427f33fafdb8fceb21b06
ClsCC <4evernaive@gmail.com>
2020-10-22 18:10:39 +0800

    Update test.py and README.md

--- README.md
+++ README.md
@@ -1,14 +1,60 @@
-# warc tools
+# warc tool (IR homework)
+
+## Note
+Coz the warc package only supports Python 2.7 and is no longer maintained, you need Python 2.7 to run warc tool. 
 
 ## Usage
-`python2 main.py filename`
-
-
+`python main.py filename`
 
 ## Input example:
-`python2 main.py 114514.warc`
-
-
+`python main.py 114514.warc`
 
 ## Output dict file:
 `output.dict`
+
+## Structure:
+Dict Object: term{}
+```json
+{
+   "wordN":{
+      "doc1":[
+         "pos1",
+         "pos2"
+      ],
+      "doc2":[
+         "pos1",
+         "pos2",
+         "pos3"
+      ],
+      "docN":[
+         "posN"
+      ]
+   }
+}
+```
+
+### Example
+
+```json
+{
+   "a":{
+      "1":[
+         3
+      ]
+   },
+   "is":{
+      "1":[
+         1
+      ],
+      "0":[
+         2
+      ]
+   },
+   "student":{
+      "1":[
+         4
+      ]
+   }
+}
+```
+
--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import json
 import numpy as np
 from collections import OrderedDict
+from collections import defaultdict
+from operator import itemgetter
+
 terms = {}
 test = [
     ["brother", "brother", "is", "my", "brother"],
@@ -22,6 +25,7 @@ for v in test:
         terms[word][str(doc_i)] = [
             i for i, e in enumerate(v) if e == word]
        # terms[word][str(doc_i)]["tf"]=terms[word][str(doc_i)]["tf"]+1
+        print terms[word].keys(),terms.keys()
     doc_i = doc_i+1
 
 for word in terms:
@@ -46,3 +50,43 @@ f = open("outtestsbput.dict", "w")
 f.write(output)
 f.close()
 # print terms.items()
+
+# Take input
+
+query = raw_input(" Enter the query : ")
+
+# Some preprocessing
+
+query = query.lower()
+query = query.strip()
+
+# now real work
+
+wordlist = query.split()
+search_words = [ x for x in wordlist if x in terms ]    # list of words that are present in index.
+
+print ("\nsearching for words ... : ", search_words, "\n")
+
+doc_has_word = [ (terms[word].keys(),word) for word in search_words ]
+doc_words = defaultdict(list)
+for d, w in doc_has_word:
+    for p in d:
+        doc_words[p].append(w)
+
+# create a dictionary identifying matches for each document    
+
+result_set = {}
+
+for i in doc_words.keys():
+    count = 0
+    matches = len(doc_words[i])     # number of matches
+    for w in doc_words[i]:
+        count += len(terms[w][i])   # count total occurances
+    result_set[i] = (matches,count)
+
+# Now print in sorted order
+
+print ("   Document \t\t Words matched \t\t Total Frequency ")
+print ('-'*40)
+for doc, (matches, count) in sorted(result_set.items(), key = itemgetter(1), reverse = True):
+    print (doc, "\t",doc_words[doc],"\t",count)
