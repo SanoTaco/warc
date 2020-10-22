ab68ddfb34242e4e5d1ce5d2b23593099acf1ce4
ClsCC <4evernaive@gmail.com>
2020-10-22 20:54:12 +0800

    Make some tests.

--- README.md
+++ README.md
@@ -65,3 +65,41 @@ Dict Object: term{}
 }
 ```
 
+## Some tests in `test.py`(for HW2):
+```python
+array = np.array(list(terms.items()))
+
+print array, "\n", array[1], "\n", array[1, 1]
+for v in array[1, 1]:
+    print v
+```
+
+The terminal will show:
+```bash
+[['sb' {'1': [3], '2': [1, 2, 3]}]
+ ['is' {'1': [1], '0': [2]}]
+ ['my' {'1': [2], '0': [3]}]
+ ['brother' {'1': [0], '0': [0, 1, 4], '2': [0]}]
+ ['student' {'1': [4], '2': [4]}]] 
+['is' {'1': [1], '0': [2]}] 
+{'1': [1], '0': [2]}
+1
+0
+```
+
+We can easily understand that `array[row]` ->`[word{docID:[pos], ... , docIDN:[posN]}] ` and `array[row][value]`->`{docID:[pos],...,docIDN:[posN]}`, it means that that `array[row]`will show all pieces of information of the word in which row and `array[row][value]` will show where this word appears in which document and position.</br>
+
+If we want to get all the positions, we can use for loop function to get them.
+
+Example:
+
+```Python
+for docid in array[row, column]:
+    print docid # To get keys
+    
+for docid in array[row, column]:
+    print array[row, column][docid] # To get values
+```
+
+
+
--- test.py
+++ test.py
@@ -7,7 +7,8 @@ from operator import itemgetter
 terms = {}
 test = [
     ["brother", "brother", "is", "my", "brother"],
-    ["brother", "is", "my", "a", "student"]
+    ["brother", "is", "my", "sb", "student"],
+    ["brother", "sb", "sb", "sb", "student"]
 ]
 
 total = {}
@@ -25,7 +26,7 @@ for v in test:
         terms[word][str(doc_i)] = [
             i for i, e in enumerate(v) if e == word]
        # terms[word][str(doc_i)]["tf"]=terms[word][str(doc_i)]["tf"]+1
-        print terms[word].keys(),terms.keys()
+        #print terms[word].keys(), terms.keys(), terms[word]
     doc_i = doc_i+1
 
 for word in terms:
@@ -34,26 +35,16 @@ for word in terms:
         amount[word] = amount[word]+len(terms[word][docid])
 
 
-    #print testOutput
+#names = ['id','data']
+#formats = ['f8','f8']
+#dtype = dict(names = names, formats=formats)
+array = np.array(list(terms.items()))
 
+print array, "\n", array[1], "\n", array[1, 1]
+for v in array[1, 1]:
+    print array[1, 1][v]
 
-#print(sorted(terms.items(), key=lambda obj: obj[0]))
-json_object = json.dumps(terms, indent=2)
-print(json_object)
-output = "\n".join("{}\t{}".format(k, v) for k, v in terms.items())
-output.replace("{", "<")
-output.replace("}", ">")
-# output1= "\n".join("{}\t{}".format(k, v) for k, v in totalTerms.items())
-# print output
-# print output
-f = open("outtestsbput.dict", "w")
-f.write(output)
-f.close()
-# print terms.items()
-
-# Take input
-
-query = raw_input(" Enter the query : ")
+query = raw_input("Query: ")
 
 # Some preprocessing
 
@@ -63,17 +54,18 @@ query = query.strip()
 # now real work
 
 wordlist = query.split()
-search_words = [ x for x in wordlist if x in terms ]    # list of words that are present in index.
+# list of words that are present in index.
+search_words = [x for x in wordlist if x in terms]
 
-print ("\nsearching for words ... : ", search_words, "\n")
+print "\nSearching for words: ", search_words, "\n"
 
-doc_has_word = [ (terms[word].keys(),word) for word in search_words ]
+doc_has_word = [(terms[word].keys(), word) for word in search_words]
 doc_words = defaultdict(list)
 for d, w in doc_has_word:
     for p in d:
         doc_words[p].append(w)
 
-# create a dictionary identifying matches for each document    
+# create a dictionary identifying matches for each document
 
 result_set = {}
 
@@ -82,11 +74,11 @@ for i in doc_words.keys():
     matches = len(doc_words[i])     # number of matches
     for w in doc_words[i]:
         count += len(terms[w][i])   # count total occurances
-    result_set[i] = (matches,count)
+    result_set[i] = (matches, count)
 
 # Now print in sorted order
 
-print ("   Document \t\t Words matched \t\t Total Frequency ")
-print ('-'*40)
-for doc, (matches, count) in sorted(result_set.items(), key = itemgetter(1), reverse = True):
-    print (doc, "\t",doc_words[doc],"\t",count)
+print "<doc#>  \t <similarity score>"
+print '-'*40
+for doc, (matches, count) in sorted(result_set.items(), key=itemgetter(1), reverse=True):
+    print doc, "  \t\t\t  ", count
